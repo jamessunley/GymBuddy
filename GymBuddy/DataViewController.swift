a6a97ba4b1db914643b51fe4e2a85e942a0d5b7f
James Sunley <jamessunley@std-wlan-172-17-117-143.sunderland.ac.uk>
2018-01-18 10:25:58 +0000

    Initial Commit

---
+++ GymBuddy/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  GymBuddy
+//
+//  Created by James Sunley on 18/01/2018.
+//  Copyright © 2018 James Sunley. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
